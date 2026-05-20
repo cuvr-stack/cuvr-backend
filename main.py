ffbcf2b44015f82d5d468ba6cedbc50715f56775
nikhilouis619 <nikhilouis619@gmail.com>
2026-05-20 15:02:58 +0400

    add app.cuvr.ae to CORS

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://reality.cuvr.ae",
+        "https://app.cuvr.ae",
         "https://cuvr-reality-dashboard.vercel.app",
         settings.frontend_url,
     ],
